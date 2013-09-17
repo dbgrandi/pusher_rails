cd6a976d61ddc7dd649805bb6e7080171a242e6f
JD Hendrickson <jd@digitalopera.com>
2013-09-17 17:02:27 -0500

    syntax error in config call

--- lib/pusher_rails.rb
+++ lib/pusher_rails.rb
@@ -1,5 +1,5 @@
 module PusherRails
   class Engine < ::Rails::Engine
-    Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'swf')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'swf')
   end
 end
