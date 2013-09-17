5090ba5b574a7e0d9e268da83b5c406ea2b97df1
JD Hendrickson <jd@digitalopera.com>
2013-09-17 17:18:34 -0500

    new attempt at setting up asset paths for the engine

--- lib/pusher_rails.rb
+++ lib/pusher_rails.rb
@@ -1,8 +1,8 @@
 module PusherRails
   class Engine < ::Rails::Engine
     # Enabling assets precompiling under rails 3.1+
-    initializer :assets do |config|
-      config.assets.paths << Rails.root.join('PusherRails', 'vendor', 'assets', 'swf')
+    initializer :append_dependent_assets_path, group: :all do |app|
+      app.config.assets.paths << 'vendor/assets/swf'
     end
   end
 end
