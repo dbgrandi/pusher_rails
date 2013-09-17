95867723c1ab1f64ca6743f32d9cbe2c83ce8659
JD Hendrickson <jd@digitalopera.com>
2013-09-17 17:09:23 -0500

    working out asset access issues for swf files

--- lib/pusher_rails.rb
+++ lib/pusher_rails.rb
@@ -1,5 +1,9 @@
 module PusherRails
   class Engine < ::Rails::Engine
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'swf')
+    # Enabling assets precompiling under rails 3.1+
+    initializer :assets do |config|
+      Rails.application.config.assets.paths << Rails.root.join('PusherRails', 'vendor', 'assets', 'swf')
+      # Rails.application.config.assets.precompile += %w( alchemy/alchemy.js alchemy/alchemy.css alchemy/print.css )
+    end
   end
 end
