f6040596d6c92dbba2de4656164130274ae5578b
JD Hendrickson <jd@digitalopera.com>
2013-09-17 17:27:52 -0500

    swf files should be auto-included by the looks of it so I'm simplifying

--- lib/pusher_rails.rb
+++ lib/pusher_rails.rb
@@ -1,8 +1,4 @@
 module PusherRails
   class Engine < ::Rails::Engine
-    # Enabling assets precompiling under rails 3.1+
-    initializer :append_dependent_assets_path, group: :all do |app|
-      app.config.assets.paths << 'vendor/assets/swf'
-    end
   end
 end
