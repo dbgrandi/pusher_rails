cc33260c0ecce7f02e7d00e0b7f3d08764fe7438
JD Hendrickson <jd@digitalopera.com>
2013-09-17 17:10:59 -0500

    assets recompilation fix

--- lib/pusher_rails.rb
+++ lib/pusher_rails.rb
@@ -2,8 +2,7 @@ module PusherRails
   class Engine < ::Rails::Engine
     # Enabling assets precompiling under rails 3.1+
     initializer :assets do |config|
-      Rails.application.config.assets.paths << Rails.root.join('PusherRails', 'vendor', 'assets', 'swf')
-      # Rails.application.config.assets.precompile += %w( alchemy/alchemy.js alchemy/alchemy.css alchemy/print.css )
+      config.assets.paths << Rails.root.join('PusherRails', 'vendor', 'assets', 'swf')
     end
   end
 end
