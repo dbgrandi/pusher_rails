527bc01a01862e6d7a67352b15a464d86bec0666
JD Hendrickson <jd@digitalopera.com>
2013-12-27 13:30:49 -0600

    added helper

--- lib/pusher_rails/helpers.rb
+++ lib/pusher_rails/helpers.rb
@@ -4,7 +4,9 @@ module PusherRails
   module Helpers
     ## Return the script tag
     def pusher_cdn_include_tag(options = {})
-      "foobar"
+      use_ssl = options.delete(:ssl) || false
+      url     = use_ssl ? URL[:https] : URL[:http]
+      javascript_include_tag url
     end
   end
 end
